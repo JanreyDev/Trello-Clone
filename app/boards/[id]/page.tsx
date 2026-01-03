8037eddb73cff90f7bf829ab6032307fcc534ccb
Janrey <janreyminadev@gmail.com>
2026-01-03 08:40:00 +0800

    Feat: Edit Color UI

--- app/boards/[id]/page.tsx
+++ app/boards/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Navbar from "@/components/navbar"
+import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -18,19 +19,70 @@ export default function BoardPage() {
     const [newTitle, setNewTitle] = useState("");
     const [newColor, setNewColor] = useState("");
 
+
+    async function handleUpdateBoard(e: React.FormEvent) {
+        e.preventDefault();
+
+        if (!newTitle.trim() || !board) return;
+
+        try {
+
+        } catch () {
+
+        }
+    }
+
     return (
         <div className="min-h-screen bg-gray-50">
             <Navbar boardTitle={board?.title} onEditBoard={() => {
+                setNewTitle(board?.title ?? "");
+                setNewColor(board?.color ?? "");
                 setIsEditingTitle(true)
             }} />
             <Dialog open={isEditingTitle} onOpenChange={setIsEditingTitle}>
                 <DialogContent className="w-[95vw] max-w-[425px] mx-auto">
                     <DialogHeader>
                         <DialogTitle>Edit Board</DialogTitle>
-                        <form>
-                            <div>
-                                <Label>Board Title</Label>
-                                <Input id="boardTitle" placeholder="Enter Board Title..." required></Input>
+                        <form className="space-y-4" onSubmit={handleUpdateBoard}>
+                            <div className="space-y-2">
+                                <Label htmlFor="boardTitle">Board Title</Label>
+                                <Input
+                                    id="boardTitle"
+                                    value={newTitle}
+                                    onChange={(e) => setNewTitle(e.target.value)}
+                                    placeholder="Enter Board Title..."
+                                    required></Input>
+                            </div>
+
+                            <div className="space-y-2">
+                                <Label>Board Color</Label>
+                                <div className="grid grid-cols-4 sm:grid-col-6 gap-2">
+                                    {
+                                        [
+                                            "bg-blue-500",
+                                            "bg-green-500",
+                                            "bg-yellow-500",
+                                            "bg-red-500",
+                                            "bg-purple-500",
+                                            "bg-pink-500",
+                                            "bg-indigo-500",
+                                            "bg-gray-500",
+                                            "bg-orange-500",
+                                            "bg-teal-500",
+                                            "bg-cyan-500",
+                                            "bg-emerald-500",
+                                        ].map((color) => (
+                                            < button type="button" className={`w-8 h-8 rounded-full ${color} ${color === newColor ? "ring-2 ring-offset-2 ring-gray-900" : ""}`}
+                                                onClick={() => setNewColor(color)}
+                                            />
+                                        ))
+                                    }
+                                </div>
+                            </div>
+
+                            <div className="flex justify-end space-x-2">
+                                <Button type="button" variant="outline" onClick={() => setIsEditingTitle(false)}>Cancel</Button>
+                                <Button type="submit">Save Changes</Button>
                             </div>
                         </form>
                     </DialogHeader>
