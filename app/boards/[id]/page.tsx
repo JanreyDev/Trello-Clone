0b7f8329c5be815df4475a8be9d032925c4e8411
Janrey <janreyminadev@gmail.com>
2026-01-03 08:10:00 +0800

    Feat: Added Dialog For edit title

--- app/boards/[id]/page.tsx
+++ app/boards/[id]/page.tsx
@@ -1,17 +1,41 @@
 "use client";
 
 import Navbar from "@/components/navbar"
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { useBoard } from "@/lib/hooks/userBoards"
+import { DialogTitle } from "@radix-ui/react-dialog";
 import { useParams } from "next/navigation";
+import { useState } from "react";
 
 
 export default function BoardPage() {
     const { id } = useParams<{ id: string }>();
     const { board } = useBoard(id);
 
+    const [isEditingTitle, setIsEditingTitle] = useState(false);
+    const [newTitle, setNewTitle] = useState("");
+    const [newColor, setNewColor] = useState("");
+
     return (
         <div className="min-h-screen bg-gray-50">
-            <Navbar boardTitle={board?.title} />
+            <Navbar boardTitle={board?.title} onEditBoard={() => {
+                setIsEditingTitle(true)
+            }} />
+            <Dialog open={isEditingTitle} onOpenChange={setIsEditingTitle}>
+                <DialogContent className="w-[95vw] max-w-[425px] mx-auto">
+                    <DialogHeader>
+                        <DialogTitle>Edit Board</DialogTitle>
+                        <form>
+                            <div>
+                                <Label>Board Title</Label>
+                                <Input id="boardTitle" placeholder="Enter Board Title..." required></Input>
+                            </div>
+                        </form>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
         </div>
     )
 }
